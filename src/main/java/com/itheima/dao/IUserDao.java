4ecf062284c7f0b116271a1dd6d912af416f3bbc
YuDongZhang <977872239@qq.com>
2021-07-01 10:04:36 +0800

    2,单表的增删改查已经完成

--- src/main/java/com/itheima/dao/IUserDao.java
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -1,6 +1,8 @@
 package com.itheima.dao;
 
 import com.itheima.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,8 +14,56 @@ public interface IUserDao {
 
     /**
      * 查询所有用户
+     *
      * @return
      */
     @Select("select * from user")
     List<User> findAll();
+
+    /**
+     * 保存用户
+     *
+     * @param user
+     */
+    @Insert("insert into user(username,address,sex,birthday) values (#{username},#{address},#{sex},#{birthday})")
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    @Insert("update user set username=#{username},address=#{address},sex=#{sex},birthday=#{birthday} where id=#{id}")
+    void updateUser(User user);
+
+    /**
+     * 删除用户
+     * @param id
+     */
+    @Delete("delete from user where id=#{id}")
+    void deleteUser(Integer id);
+
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User findById(Integer id);
+
+    /**
+     * 根据名称查询用户
+     * @return
+     */
+    @Select("select * from user where username like #{username}")
+    List<User> findByName(String name);
+
+    /**
+     * 根据名称查询用户,第二中方式
+     * @return
+     */
+    @Select("select * from user where username like '%${value}%' ")
+    List<User> findByUserName(String name);
+
+    @Select("select count(*) from user")
+    int findTotalUser();
 }
--- src/test/java/com/itheima/test/AnnoTest.java
+++ src/test/java/com/itheima/test/AnnoTest.java
@@ -6,30 +6,123 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 public class AnnoTest {
 
-    public static void main(String[] args) throws IOException {
-//1.读取配置文件
-        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-        //2.创建 sqlSessionFactory 工厂
-        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory factory = builder.build(in);
-        //3.使用工厂生成 sqlSession对象
-        SqlSession session = factory.openSession();
-        //4.使用 sqlsession创建Dao接口的代理对象
-        IUserDao userDao = session.getMapper(IUserDao.class);
-        //5.使用代理对象执行 方法
+    private InputStream in;
+    private SqlSession sqlSession;
+    private IUserDao userDao;
+
+    @Before//用于在测试方法执行之前执行
+    public void init() throws Exception {
+        //1.读取配置文件，生成字节输入流
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.获取SqlSessionFactory
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        //3.获取SqlSession对象
+        sqlSession = factory.openSession(true);//改成true是可以自动提交
+        //4.获取dao的代理对象
+        userDao = sqlSession.getMapper(IUserDao.class);
+    }
+
+    @After//用于在测试方法执行之后执行
+    public void destroy() throws Exception {
+        //提交事务
+//        sqlSession.commit();
+        //6.释放资源
+        sqlSession.close();
+        in.close();
+    }
+
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public void testFindAll() {
         List<User> users = userDao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
-        //6.释放资源
-        session.close();
-        in.close();
+    }
+
+    /**
+     * 保存用户
+     */
+    @Test
+    public void testSaveUser() {
+        User user = new User();
+        user.setUsername("张三");
+        user.setAddress("南京");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        userDao.saveUser(user);
+    }
+
+    /**
+     * 更新用户
+     */
+    @Test
+    public void testUpdateUser() {
+        User user = new User();
+        user.setUsername("老王");
+        user.setAddress("南京");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        user.setId(49);
+        userDao.updateUser(user);
+    }
+
+    /**
+     * 删除用户
+     */
+    @Test
+    public void testDeleteUser() {
+        userDao.deleteUser(49);
+    }
+
+    /**
+     * 根据id查用户
+     */
+    @Test
+    public void testFindById() {
+        System.out.println(userDao.findById(48));
+    }
+
+    /**
+     * 根据姓名查用户
+     */
+    @Test
+    public void testFindByName() {
+        List<User> users = userDao.findByName("%王%");
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    /**
+     * 根据姓名查用户
+     */
+    @Test
+    public void testFindByTwoName() {
+        List<User> users = userDao.findByUserName("王");
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    /**
+     * 根据姓名查用户
+     */
+    @Test
+    public void testTotalUser() {
+        System.out.println(userDao.findTotalUser());
     }
 }
